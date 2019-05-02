a6a8543bde389a98c92f646bdd9910134f3c1ab3
eduardo <mm@gg.com>
2019-05-02 17:03:17 -0500

    Se adiciona a clase H2MemoryDatabase los metodos necesarios para realizar un CRUD

--- src/main/java/com/test/prueba/ejemplo/App.java
+++ src/main/java/com/test/prueba/ejemplo/App.java
@@ -1,13 +1,26 @@
 package com.test.prueba.ejemplo;
 
+import java.sql.SQLException;
+
+import com.test.pruebas.persistence.H2MemoryDatabase;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws SQLException
     {
         System.out.println( "Hello World!" );
+        H2MemoryDatabase db = new H2MemoryDatabase();
+        
+        if(db.CreateTableWithPrepStatement()) {
+        	System.out.println( "Create Table As Succesful!" );
+        	db.insertWithPreparedStatement(1, "Luis Eduardo Rosales Prieto.");
+        }
+        else {
+        	db.closeConnection();
+        }
     }
 }
--- src/main/java/com/test/pruebas/persistence/H2MemoryDatabase.java
+++ src/main/java/com/test/pruebas/persistence/H2MemoryDatabase.java
@@ -2,6 +2,8 @@ package com.test.pruebas.persistence;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class H2MemoryDatabase {
@@ -12,8 +14,7 @@ public class H2MemoryDatabase {
 	private static final String DB_PASSWORD = "";
 	private Connection dbConnection = null;
 
-	public H2MemoryDatabase(Connection dbConnection) {
-		this.dbConnection = dbConnection;
+	public H2MemoryDatabase() {
 		try {
 			Class.forName(DB_DRIVER);
 		} catch (ClassNotFoundException e) {
@@ -23,6 +24,101 @@ public class H2MemoryDatabase {
 			this.dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
+		}
+	}
+
+	public boolean CreateTableWithPrepStatement() throws SQLException {
+		PreparedStatement createPreparedStatement = null;
+		String CreateQuery = "CREATE TABLE PERSON(id int primary key, name varchar(255))";
+		try {
+			this.dbConnection.setAutoCommit(false);
+
+			createPreparedStatement = this.dbConnection.prepareStatement(CreateQuery);
+			createPreparedStatement.executeUpdate();
+			createPreparedStatement.close();
+
+			System.out.println("H2 In-Memory Database Created Succes");
+			this.dbConnection.commit();
+		} catch (SQLException e) {
+			System.out.println("Exception Message " + e.getLocalizedMessage());
+			System.out.println("H2 In-Memory Database Created Failed");
+			return false;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("H2 In-Memory Database Created Failed");
+			return false;
+		}
+
+		return true;
+	}
+
+	public boolean insertWithPreparedStatement(int cod, String name) throws SQLException {
+		PreparedStatement insertPreparedStatement = null;
+
+		String InsertQuery = "INSERT INTO PERSON" + "(id, name) values" + "(?,?)";
+
+		try {
+			this.dbConnection.setAutoCommit(false);
+
+			insertPreparedStatement = this.dbConnection.prepareStatement(InsertQuery);
+			insertPreparedStatement.setInt(1, cod);
+			insertPreparedStatement.setString(2, name);
+			insertPreparedStatement.executeUpdate();
+			insertPreparedStatement.close();
+			System.out.println("H2 In-Memory Database inserted through PreparedStatement");
+
+			this.dbConnection.commit();
+		} catch (SQLException e) {
+			System.out.println("H2 In-Memory Database Inserted Failed");
+			return false;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("H2 In-Memory Database Inserted Failed");
+			return false;
+		}
+
+		return true;
+	}
+
+	public boolean selectWithPreparedStatement() throws SQLException {
+		PreparedStatement selectPreparedStatement = null;
+
+		String SelectQuery = "select * from PERSON";
+
+		try {
+			this.dbConnection.setAutoCommit(false);
+
+			selectPreparedStatement = this.dbConnection.prepareStatement(SelectQuery);
+			ResultSet rs = selectPreparedStatement.executeQuery();
+			System.out.println("H2 In-Memory Database Select through PreparedStatement");
+			while (rs.next()) {
+				System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"));
+			}
+			selectPreparedStatement.close();
+
+			this.dbConnection.commit();
+		} catch (SQLException e) {
+			System.out.println("H2 In-Memory Database Inserted Failed");
+			return false;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("H2 In-Memory Database Inserted Failed");
+			return false;
+		}
+
+		return true;
+	}
+
+	public void closeConnection() {
+		try {
+			this.dbConnection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("H2 In-Memory Database Closed  Failed");
 		}
 	}
 
